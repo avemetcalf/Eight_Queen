48638d3732650f04eced6b4e691a0572fbb933d0
avemetcalf <100744834+avemetcalf@users.noreply.github.com>
2022-07-08 10:40:08 -0400

    Update EightQueen.java

--- EightQueen.java
+++ EightQueen.java
@@ -1,1 +1,44 @@
 
+public class EightQueen {
+
+	 private int row;
+	    private int column;
+
+	    //Constructors
+	    public EightQueen(int row, int column) {
+	        this.row = row;
+	        this.column = column;
+	    }
+	    
+
+	    public void move () {
+	        row++;
+	    }
+	    
+
+	    public boolean ifConflict(EightQueen queen){
+	        
+	        if(row == queen.getRow() || column == queen.getColumn()){
+	            return true;
+	            }
+	           
+	        else if(Math.abs(column-queen.getColumn()) == Math.abs(row-queen.getRow())){
+	            return true;
+	            }
+	        else{
+	            return false;
+	        }
+	    }
+	    
+
+	    public int getRow() {
+	        return row;
+	    }
+
+
+	    public int getColumn() {
+	        return column;
+	    }
+	    
+	
+}
